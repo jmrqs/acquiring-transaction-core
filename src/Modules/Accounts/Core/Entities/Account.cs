e746fbb5fe0dc938c13505fa9b5a9d2aee41b578
jmrqs <74171400+jmrqs@users.noreply.github.com>
2025-06-04 02:03:27 -0300

    feat: updated docker compose; miscellaneous changes

---
+++ src/Modules/Accounts/Core/Entities/Account.cs
@@ -0,0 +1,15 @@
+﻿using Module.Accounts.Core.ValueObjects.Enums;
+
+namespace Module.Accounts.Core.Entities
+{
+    public class Account
+    {
+        public Guid CustomerId { get; set; }
+        public Guid AccountId { get; set; }
+        public int AccountNumber { get; set; }
+        public decimal AvailableBalance { get; set; }
+        public decimal ReservedBalance { get; set; }
+        public decimal CreditLimit { get; set; }
+        public AccountStatus AccountStatus { get; set; }
+    }
+}
---
+++ src/Modules/Accounts/Core/ValueObjects/Enums/AccountStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace Module.Accounts.Core.ValueObjects.Enums
+{
+    public enum AccountStatus
+    {
+        Inactive = 0,
+        Active = 1
+    }
+}
